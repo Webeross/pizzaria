4d74000622a07ac82e6146fb91f6fa0d83e6560c
Webeross <edu_weber_dias@hotmail.com>
2024-04-30 14:02:49 -0300

    ajuste

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
-const PORT = 5000;
+const PORT = 3500;
 
 const app = express();
 
@@ -9,27 +9,6 @@ app.use(express.static('public'));
 
 app.get('/index', (req, res) => {
     fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
-        if(err){
-            res.status(500).send("500 - erro do servidor");
-        }else{
-            res.status(200).type('text/html').send(data);
-        }
-    })
-
-})
-app.get('/cardapio', (req, res) => {
-    fs.readFile(path.join(__dirname, 'cardapio.html'), (err, data) => {
-        if(err){
-            res.status(500).send("500 - erro do servidor");
-        }else{
-            res.status(200).type('text/html').send(data);
-        }
-    })
-
-})
-
-app.get('/pedido', (req, res) => {
-    fs.readFile(path.join(__dirname, 'pedido.html'), (err, data) => {
         if(err){
             res.status(500).send("500 - erro do servidor");
         }else{
