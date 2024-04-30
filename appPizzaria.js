d2b352f90df082b48f5014e35df44ec46773d3fb
VitorMartinsAlves <vitormartinsalves99@gmail.com>
2024-04-30 13:30:21 -0300

    fix

--- appPizzaria.js
+++ appPizzaria.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
 
-    function montarPizza(){
+    function montarPizza() {
 
         let custo = 0;
         let extra = "";
@@ -11,15 +11,15 @@ document.addEventListener("DOMContentLoaded", function(){
         let saborSelecionado = document.querySelector('input[name="sabor"]:checked');
         let sabor = saborSelecionado ? saborSelecionado.value : 'Sabor não selecionado';
         console.log(sabor)
-        
+
         // Obtendo tamanho selecionado 
         let tamanhoSelecionado = document.querySelector('input[name="tamanho"]:checked');
         let tamanho = tamanhoSelecionado ? tamanhoSelecionado.value : 'Tamanho não selecionado';
-        
+
         // Borda selecionada
         let bordaSelecionada = document.querySelector('input[name="borda"]:checked');
         let borda = bordaSelecionada ? bordaSelecionada.value : 'Borda não selecionada';
-        
+
         // Ingredientes Extras
         let queijoExtraCheckbox = document.querySelector('input[name="extra-queijo"]:checked');
         if (queijoExtraCheckbox) {
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function(){
             extra += "<br>   - Pepperoni";
         }
         descricao += extra;
-        
+
         // Opcionais
         let oreganoOpcionalCheckbox = document.querySelector('input[name="opcionais-oregano"]:checked');
         if (oreganoOpcionalCheckbox) {
@@ -55,18 +55,18 @@ document.addEventListener("DOMContentLoaded", function(){
         if (pimentaOpcionalCheckbox) {
             opcionais += "<br>   - Pimenta";
         }
-        
+
         // Concatenando descrição
         descricao += opcionais;
-        
+
         // Montando a descrição completa
         descricao = "<br>    - " + sabor + "<br>    - " + tamanho + "<br>    - " + borda + extra + opcionais;
 
-        if ((sabor === 'margherita' || sabor === 'calabresa' || sabor === 'Frango c/ Catupiry') && tamanho === 'brotinho') {
+        if ((sabor === 'margherita' || sabor === 'calabresa' || sabor === 'frango') && tamanho === 'brotinho') {
             custo = 18.00;
-        } else if ((sabor === 'margherita' || sabor === 'calabresa' || sabor === 'Frango c/ Catupiry') && tamanho === 'padrao') {
+        } else if ((sabor === 'margherita' || sabor === 'calabresa' || sabor === 'frango') && tamanho === 'padrao') {
             custo = 25.00;
-        } else if ((sabor === 'margherita' || sabor === 'calabresa' || sabor === 'Frango c/ Catupiry') && tamanho === 'grande') {
+        } else if ((sabor === 'margherita' || sabor === 'calabresa' || sabor === 'frango') && tamanho === 'grande') {
             custo = 31.00;
         } else if (sabor === 'portuguesa' && tamanho === 'brotinho') {
             custo = 24.00;
@@ -74,11 +74,11 @@ document.addEventListener("DOMContentLoaded", function(){
             custo = 32.00;
         } else if (sabor === 'portuguesa' && tamanho === 'grande') {
             custo = 40.00;
-        } else if (sabor === '4 Queijos' && tamanho === 'brotinho') {
+        } else if (sabor === 'queijos' && tamanho === 'brotinho') {
             custo = 26.00;
-        } else if (sabor === '4 Queijos' && tamanho === 'padrao') {
+        } else if (sabor === 'queijos' && tamanho === 'padrao') {
             custo = 35.00;
-        } else if (sabor === '4 Queijos' && tamanho === 'grande') {
+        } else if (sabor === 'queijos' && tamanho === 'grande') {
             custo = 43.75;
         }
 
@@ -86,10 +86,10 @@ document.addEventListener("DOMContentLoaded", function(){
         // Atualizando o custo total
         let totalPedidoElement = document.getElementById("total-pedido");
         totalPedidoElement.textContent = "Total do Pedido: R$ " + custo.toFixed(2);
-        
+
         // Atualizando a descrição do pedido
         let descricaoPedidoElement = document.getElementById("descricao-pedido");
-        descricaoPedidoElement.innerHTML = descricao; 
+        descricaoPedidoElement.innerHTML = descricao;
     }
 
     // Chama a função montarPizza ao clicar no botão
